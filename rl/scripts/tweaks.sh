bec807f7e671cbb55f07db7ad59b94a2bdad6654
Stancu Florin <niflostancu@gmail.com>
2019-11-11 14:09:29 +0200

    add tmux config

--- rl/scripts/tweaks.sh
+++ rl/scripts/tweaks.sh
@@ -18,14 +18,20 @@ rsync -avh --chown="root:root" "$SRC/files/opt/" /opt/
 chmod 755 /etc/rc.local
 chmod 755 /etc/network/interfaces
 
-# bashrc
-_copy_bashrc() {
+# user home configs
+_copy_home_config() {
+	# copy & enable bashrc:
 	cp -f "$SRC/files/home/bashrc" "$1"/.bashrc
 	chmod 755 "$1"/.bashrc
 	chown "$2:$2" "$1"/.bashrc
+	# copy tmux config
+	mkdir -p "$1"/.config/tmux/
+	rsync -avh --chown="$2:$2" "$SRC/files/home/tmux/" "$1/.config/tmux/"
+	ln -s "$1/.config/tmux/tmux.conf" "$1/.tmux.conf"
+	chown "$2:$2" "$1/.tmux.conf"
 }
-_copy_bashrc /root root
-_copy_bashrc /home/student student
+_copy_home_config /root root
+_copy_home_config /home/student student
 
 # Use old interface names (ethX)
 sed -i "s/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"net.ifnames=0 biosdevname=0\"/g" /etc/default/grub
