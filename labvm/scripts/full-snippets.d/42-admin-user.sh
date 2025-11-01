3fa1a0d308ea8e19dc7eb1c1921aa6eb343650e3
Stancu Florin <niflostancu@gmail.com>
2025-11-01 15:40:45 +0200

    admin: install dotfiles

--- labvm/scripts/full-snippets.d/42-admin-user.sh
+++ labvm/scripts/full-snippets.d/42-admin-user.sh
@@ -14,3 +14,13 @@ echo 'admin ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/admin
 # Note: this will be changed in private cloud VMs
 echo "admin:admin1337" | chpasswd
 
+# install labvm-dotfiles for 'admin' user, too
+function _install_admin_config() {
+	set -e
+	# no need for nvim config
+	"$RL_SRC/files/labvm-dotfiles/install.sh" -nvim
+}
+_exported_script="$(declare -p RL_SRC); $(declare -f _install_admin_config)"
+chsh -s /usr/bin/zsh "admin"
+echo "$_exported_script; _install_admin_config" | su -c bash "admin"
+
