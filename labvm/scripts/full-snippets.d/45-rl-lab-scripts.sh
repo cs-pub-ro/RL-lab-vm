8e642bbadff15f201b2591c66ba6b13c7358141c
Stancu Florin <niflostancu@gmail.com>
2025-11-01 15:17:58 +0200

    lab-scripts: fix git update mechanisms

--- labvm/scripts/full-snippets.d/45-rl-lab-scripts.sh
+++ labvm/scripts/full-snippets.d/45-rl-lab-scripts.sh
@@ -6,9 +6,9 @@ rsync -rvh --chown=root --chmod=755 "$RL_SRC/files/opt/rl/" /opt/rl/
 # copy the lab scripts (separate repo for upgradibility)
 LABS_SRC="$RL_SRC/thirdparty/labs"
 LABS_DEST="/opt/rl-labs"
-if [[ ! -f "$LABS_SRC" ]]; then
+if [[ -d "$LABS_SRC/.git" ]]; then
 	rm -rf "$LABS_DEST"
-	rsync -a --chown=root:root --mkpath "$LABS_SRC/" "$LABS_DEST/"
+	git clone "$LABS_SRC/" "$LABS_DEST"
 else
 	echo "Warning: missing '$LABS_SRC'!" \
 		"Lab scripts will NOT be present in this image..." >&2
@@ -19,7 +19,12 @@ fi
 	set -e
 	cd "$LABS_DEST"
 	./build.sh
+	[[ "$VM_DEBUG" == "1" ]] || touch ".update-required"
+	# replace git remote with HTTPS
+	@silent git remote remove origin
+	git remote add update https://github.com/cs-pub-ro/RL-linux-labs.git
 )
+
 
 # Install rl-watchdog service
 vm_run_script "$RL_SRC/files/opt/rl/watchdog/install.sh"
