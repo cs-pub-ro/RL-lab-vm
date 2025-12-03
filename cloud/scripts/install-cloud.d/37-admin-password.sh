a74a1eafb855ff31b7ab756e002ca1532d9e1384
Stancu Florin <niflostancu@gmail.com>
2025-12-03 17:09:08 +0200

    cloud: refactor bootcmd cfg + also disable ctrl-alt-del burst

--- cloud/scripts/install-cloud.d/37-admin-password.sh
+++ cloud/scripts/install-cloud.d/37-admin-password.sh
@@ -16,4 +16,6 @@ fi
 
 # disable reboot from OpenStack's console CtrlAltDel btn
 systemctl mask "ctrl-alt-del.target"
+# also disable force reboot on burst presses
+sed -i -E 's/^#?CtrlAltDelBurstAction=.*/CtrlAltDelBurstAction=none/' /etc/systemd/system.conf
 
